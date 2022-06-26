eeabb7e3cd9f67379fa714a7fd6d6bd7ecda0361
jb6400 <jb6400@nyu.edu>
2022-06-26 14:07:12 -0400

    can keep track of score

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -58,14 +58,32 @@ const char ICE_SPRITE[] = "Large_Frost_Sprite_0.png";
 const char PLAYER_1_SPRITE[] = "player-1-sprite.png";
 const char PLAYER_2_SPRITE[] = "player-2-sprite.png";
 const char SIDE_BORDER_SPRITE[] = "side-border.png";
-const char TOP_BORDER_SPRITE[] = "top-bot-border.png";
+const char BOT_BORDER_SPRITE[] = "top-bot-border.png";
+const char TOP_BORDER_SPRITE[] = "top-border.png";
+
+const char NUM_0[] = "numbers-sprite-0.png";
+const char NUM_1[] = "numbers-sprite-1.png";
+const char NUM_2[] = "numbers-sprite-2.png";
+const char NUM_3[] = "numbers-sprite-3.png";
+const char NUM_4[] = "numbers-sprite-4.png";
+const char NUM_5[] = "numbers-sprite-5.png";
+const char NUM_6[] = "numbers-sprite-6.png";
+const char NUM_7[] = "numbers-sprite-7.png";
+const char NUM_8[] = "numbers-sprite-8.png";
+const char NUM_9[] = "numbers-sprite-9.png";
 
 GLuint ice_texture_id, player_1_texture_id, player_2_texture_id,
-	   side_border_texture_id, top_border_texture_id;
+	   side_border_texture_id, bot_border_texture_id, top_border_texture_id,
+	   num_0_texture_id, num_1_texture_id, num_2_texture_id, num_3_texture_id,
+	   num_4_texture_id, num_5_texture_id, num_6_texture_id, num_7_texture_id,
+	   num_8_texture_id, num_9_texture_id;
 
 const int NUMBER_OF_TEXTURES = 1; // to be generated, that is
 const GLint LEVEL_OF_DETAIL = 0;  // base image level; Level n is the nth mipmap reduction image
 const GLint TEXTURE_BORDER = 0;   // this value MUST be zero
+
+int SCORE_P1 = 0;
+int SCORE_P2 = 0;
 
 GLuint load_texture(const char* filepath) {
 	int width, height, number_of_components;
@@ -81,8 +99,8 @@ GLuint load_texture(const char* filepath) {
 	glBindTexture(GL_TEXTURE_2D, textureID);
 	glTexImage2D(GL_TEXTURE_2D, LEVEL_OF_DETAIL, GL_RGBA, width, height, TEXTURE_BORDER, GL_RGBA, GL_UNSIGNED_BYTE, image);
 
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
 	stbi_image_free(image);
 
@@ -110,7 +128,19 @@ void initialise() {
 	player_1_texture_id = load_texture(PLAYER_1_SPRITE);
 	player_2_texture_id = load_texture(PLAYER_2_SPRITE);
 	side_border_texture_id = load_texture(SIDE_BORDER_SPRITE);
+	bot_border_texture_id = load_texture(BOT_BORDER_SPRITE);
 	top_border_texture_id = load_texture(TOP_BORDER_SPRITE);
+
+	num_0_texture_id = load_texture(NUM_0);
+	num_1_texture_id = load_texture(NUM_1);
+	num_2_texture_id = load_texture(NUM_2);
+	num_3_texture_id = load_texture(NUM_3);
+	num_4_texture_id = load_texture(NUM_4);
+	num_5_texture_id = load_texture(NUM_5);
+	num_6_texture_id = load_texture(NUM_6);
+	num_7_texture_id = load_texture(NUM_7);
+	num_8_texture_id = load_texture(NUM_8);
+	num_9_texture_id = load_texture(NUM_9);
 
 	// Init view, model, and proj. matricies
 	view_matrix = glm::mat4(1.f); // Identify 4x matrix
@@ -121,14 +151,13 @@ void initialise() {
 	model_matrix_border = glm::mat4(1.f); // Identify 4x matrix
 
 	projection_matrix = glm::ortho(-5.f, 5.f, -3.75f, 3.75f, -1.f, 1.f);
-	//projection_matrix = glm::ortho(-5.0f, 5.0f, -3.75f, 3.75f, -1.0f, 1.0f);
 	//Camera looks perp on triangle
 
 	//Load them onto OpenGL
 	program.SetViewMatrix(view_matrix);
 	program.SetProjectionMatrix(projection_matrix);
 
-	//Set color of the triagle
+	//Set color of the triangle
 	program.SetColor(1.0f, 0.44f, 0.38f, 1.0f);
 
 	glUseProgram(program.programID);
@@ -199,21 +228,21 @@ bool check_collision(glm::vec3& position_1, glm::vec3& position_2, const float& 
 	return sqrt(pow(position_2[0] - position_1[0], 2) + 
 		   pow(position_2[1] - position_1[1], 2)) < collsion_dist;
 }
-bool check_collision2(glm::vec3& position_1, glm::vec3& position_2, const float& collsion_dist) {
+/*bool check_collision2(glm::vec3& position_1, glm::vec3& position_2, const float& collsion_dist) {
 	_RPTF2(_CRT_WARN, "check_collision2 returns x: %f\n", sqrt(pow(position_2[0] - position_1[0], 2)));
 	_RPTF2(_CRT_WARN, "check_collision2 returns y: %f\n", sqrt(pow(position_2[1] - position_1[1], 2)));
 	return sqrt(pow(position_2[0] - position_1[0], 2) +
 		pow(position_2[1] - position_1[1], 2)) < collsion_dist;
 	//return (sqrt(pow(position_2[0] - position_1[0], 2)) < MINIMUM_COLLISION_DISTANCE) ||
 	//	(sqrt(pow(position_2[1] - position_1[1], 2)) < MINIMUM_COLLISION_DISTANCE);
-}
+}*/
 void update() {
 	//for delta time
 	float ticks = (float)SDL_GetTicks() / 1000.f; // get the current number of ticks
 	float delta_time = ticks - previous_ticks; // the delta time is the difference from the last frame
 	previous_ticks = ticks;
 
-	glm::vec3 top_border_vec = glm::vec3(0, 3.625f, 0);
+	glm::vec3 top_border_vec = glm::vec3(0, 3.425f, 0);
 	glm::vec3 bot_border_vec = glm::vec3(0, -3.625f, 0);
 
 	glm::vec3 right_border_vec = glm::vec3(4.875f, 0, 0);
@@ -233,14 +262,23 @@ void update() {
 			BALL_Y_TRANS = -1.f * BALL_Y_TRANS;
 		}
 
-		if (check_collision(b_m_x,right_border_vec, MINIMUM_COLLISION_DISTANCE_BALL) ||
-			check_collision(b_m_x, left_border_vec, MINIMUM_COLLISION_DISTANCE_BALL))
+		bool collision_right = check_collision(b_m_x, right_border_vec, MINIMUM_COLLISION_DISTANCE_BALL);
+		bool collision_left = check_collision(b_m_x, left_border_vec, MINIMUM_COLLISION_DISTANCE_BALL);
+
+		if (collision_right || collision_left)
 		{
 			//add lose condition
 			//BALL_X_TRANS = -1.f * BALL_X_TRANS;
 			BALL_X_TRANS = 0.f;
 			BALL_Y_TRANS = 0.f;
 			is_game_over = true;
+			if (collision_right) {
+				SCORE_P1++;
+			}
+			else {
+				SCORE_P2++;
+			}
+			ball_moving = false;
 		}
 
 		if (check_collision(ball_position, player1_global_pos, MINIMUM_COLLISION_DISTANCE_BALL) ||
@@ -295,6 +333,43 @@ void draw_object(glm::mat4& object_model_matrix, GLuint& object_texture_id)
 	glBindTexture(GL_TEXTURE_2D, object_texture_id);
 	glDrawArrays(GL_TRIANGLES, 0, 6); // we are now drawing 2 triangles, so we use 6 instead of 3
 }
+GLuint sprite_calc(int expression) {
+	switch (expression) {
+	case 0:
+		return num_0_texture_id;
+		break;
+	case 1:
+		return num_1_texture_id;
+		break;
+	case 2:
+		return num_2_texture_id;
+		break;
+	case 3:
+		return num_3_texture_id;
+		break;
+	case 4:
+		return num_4_texture_id;
+		break;
+	case 5:
+		return num_5_texture_id;
+		break;
+	case 6:
+		return num_6_texture_id;
+		break;
+	case 7:
+		return num_7_texture_id;
+		break;
+	case 8:
+		return num_8_texture_id;
+		break;
+	case 9:
+		return num_9_texture_id;
+		break;
+	default:
+		return num_0_texture_id;
+		break;
+	}
+}
 void render() {
 	//STEP 1
 	glClear(GL_COLOR_BUFFER_BIT);
@@ -345,8 +420,8 @@ void render() {
 
 	float vertices_b_top[] = 
 	{
-		-5.f, 2.85f, 5.f, 2.85f, 5.f, 3.75f, //triangle1
-		-5.f, 2.85f, 5.f, 3.75f, -5.f, 3.75f   //triangle2
+		-5.f, 2.65f, 5.f, 2.65f, 5.f, 3.75f, //triangle1
+		-5.f, 2.65f, 5.f, 3.75f, -5.f, 3.75f   //triangle2
 	};
 	float vertices_b_right[] = 
 	{
@@ -391,7 +466,7 @@ void render() {
 	glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, texture_coordinates);
 	glEnableVertexAttribArray(program.texCoordAttribute);
 
-	draw_object(model_matrix_border, top_border_texture_id);
+	draw_object(model_matrix_border, bot_border_texture_id);
 
 	glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, vertices_b_left);
 	glEnableVertexAttribArray(program.positionAttribute);
@@ -419,7 +494,115 @@ void render() {
 
 	draw_object(model_matrix_b, ice_texture_id);
 
+	//score 
+	program.SetModelMatrix(model_matrix_border);
 
+	//player 1 score
+
+	GLuint sprite_num_sp1_0, sprite_num_sp1_1, sprite_num_sp1_2;
+
+	sprite_num_sp1_0 = sprite_calc(SCORE_P1 % 10);
+	sprite_num_sp1_1 = sprite_calc((SCORE_P1 / 10) % 10);
+	sprite_num_sp1_2 = sprite_calc(SCORE_P1 / 100);
+
+	float vertices_sp1_0[] =
+	{
+		-2.2f, 3.35f, -1.95f, 3.35f, -1.95f, 3.65f,  //triangle1
+		-2.2f, 3.35, -1.95f, 3.65f, -2.2f, 3.65f  //triangle2
+	};
+
+	float vertices_sp1_1[] =
+	{
+		-1.85f, 3.35f, -1.6f, 3.35f, -1.6f, 3.65f,  //triangle1
+		-1.85f, 3.35, -1.6f, 3.65f, -1.85f, 3.65f  //triangle2
+	};
+
+	float vertices_sp1_2[] =
+	{
+		-1.5f, 3.35f, -1.25f, 3.35f, -1.25f, 3.65f,  //triangle1
+		-1.5f, 3.35, -1.25f, 3.65f, -1.5f, 3.65f  //triangle2
+	};
+
+	//first number
+	glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, vertices_sp1_0);
+	glEnableVertexAttribArray(program.positionAttribute);
+
+	glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, texture_coordinates);
+	glEnableVertexAttribArray(program.texCoordAttribute);
+
+	draw_object(model_matrix_border, sprite_num_sp1_2);
+
+	//second number
+	glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, vertices_sp1_1);
+	glEnableVertexAttribArray(program.positionAttribute);
+
+	glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, texture_coordinates);
+	glEnableVertexAttribArray(program.texCoordAttribute);
+
+	draw_object(model_matrix_border, sprite_num_sp1_1);
+
+	//third number
+	glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, vertices_sp1_2);
+	glEnableVertexAttribArray(program.positionAttribute);
+
+	glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, texture_coordinates);
+	glEnableVertexAttribArray(program.texCoordAttribute);
+
+	draw_object(model_matrix_border, sprite_num_sp1_0);
+
+	//player 2 score 
+
+	GLuint sprite_num_sp2_0, sprite_num_sp2_1, sprite_num_sp2_2;
+
+	sprite_num_sp2_0 = sprite_calc(SCORE_P2 % 10);
+	sprite_num_sp2_1 = sprite_calc((SCORE_P2 / 10) % 10);
+	sprite_num_sp2_2 = sprite_calc(SCORE_P2 / 100);
+	
+	float vertices_sp2_0[] =
+	{
+		3.4f, 3.35f, 3.65f, 3.35f, 3.65f, 3.65f,  //triangle1
+		3.4f, 3.35, 3.65f, 3.65f, 3.4f, 3.65f  //triangle2
+	};
+
+	float vertices_sp2_1[] =
+	{
+		3.75f, 3.35f, 4.f, 3.35f, 4.f, 3.65f,  //triangle1
+		3.75f, 3.35, 4.f, 3.65f, 3.75f, 3.65f  //triangle2
+	};
+
+	float vertices_sp2_2[] =
+	{
+		4.1f, 3.35f, 4.35f, 3.35f, 4.35f, 3.65f,  //triangle1
+		4.1f, 3.35, 4.35f, 3.65f, 4.1f, 3.65f  //triangle2
+	};
+
+	//first number
+	glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, vertices_sp2_0);
+	glEnableVertexAttribArray(program.positionAttribute);
+
+	glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, texture_coordinates);
+	glEnableVertexAttribArray(program.texCoordAttribute);
+
+	draw_object(model_matrix_border, sprite_num_sp2_2);
+
+	//second number
+	glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, vertices_sp2_1);
+	glEnableVertexAttribArray(program.positionAttribute);
+
+	glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, texture_coordinates);
+	glEnableVertexAttribArray(program.texCoordAttribute);
+
+	draw_object(model_matrix_border, sprite_num_sp2_1);
+
+	//third number
+	glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, vertices_sp2_2);
+	glEnableVertexAttribArray(program.positionAttribute);
+
+	glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, texture_coordinates);
+	glEnableVertexAttribArray(program.texCoordAttribute);
+
+	draw_object(model_matrix_border, sprite_num_sp2_0);
+	
 	glDisableVertexAttribArray(program.positionAttribute);
 	//STEP 4
 	SDL_GL_SwapWindow(display_window);
